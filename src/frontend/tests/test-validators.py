7d1f5efe453d8a463f78556f49d2f6e1978b5f20
Heikki Nousiainen <Heikki.Nousiainen@F-Secure.com>
2013-01-28 19:52:19 +0200

    frontend: added validator for job_ids

--- src/frontend/tests/test-validators.py
+++ src/frontend/tests/test-validators.py
@@ -18,6 +18,22 @@ class TestTasks:
             '00000000-0000-/000-0000-000000000000'
         ]
 
+    valid_job_ids = [
+            'myjob',
+            'my_job',
+            'my-job-2',
+            'abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789'
+        ]
+
+    invalid_job_ids = [
+            '/etc/passwd',
+            '..',
+            '/',
+            'my+job',
+            '(my)job',
+            'my:job'
+        ]
+
     def test_01_valid_task_ids(self):
         for task_id in self.valid_task_ids:
             assert validators.validate_task_id(task_id) == task_id, "Validator declined valid task_id %s" % task_id
@@ -25,4 +41,12 @@ class TestTasks:
     def test_02_invalid_task_ids(self):
         for task_id in self.invalid_task_ids:
             assert validators.validate_task_id(task_id) is None, "Validator accepted invalid task_id %s" % task_id
+
+    def test_03_valid_job_ids(self):
+        for job_id in self.valid_job_ids:
+            assert validators.validate_job_id(job_id) == job_id, "Validator declined valid job_id %s" % job_id
+
+    def test_04_invalid_job_ids(self):
+        for job_id in self.invalid_job_ids:
+            assert validators.validate_job_id(job_id) is None, "Validator accepted invalid job_id %s" % task_id
 
--- src/frontend/validators.py
+++ src/frontend/validators.py
@@ -3,10 +3,21 @@
 import re
 
 __TASK_ID_VALIDATOR = re.compile('^([a-f0-9]{8})-([a-f0-9]{4})-([a-f0-9]{4})-([a-f0-9]{4})-([a-f0-9]{12})$')
+__JOB_ID_VALIDATOR = re.compile('^([a-zA-Z0-9-_]+)$')
+__JOB_ID_MAX_LEN = 256
 
 def validate_task_id(task_id):
     """ Validate task ID """
     matches = __TASK_ID_VALIDATOR.match(task_id)
+    if matches is not None:
+        return matches.group(0)
+    return None
+
+def validate_job_id(job_id):
+    """ Validate job ID """
+    if len(job_id) > __JOB_ID_MAX_LEN:
+        return None
+    matches = __JOB_ID_VALIDATOR.match(job_id)
     if matches is not None:
         return matches.group(0)
     return None
