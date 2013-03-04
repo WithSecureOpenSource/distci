53955ce51a9a35f156ed6f46d31932f02ca37e75
Heikki Nousiainen <Heikki.Nousiainen@F-Secure.com>
2013-03-04 10:29:45 +0200

    frontend: common constants module

---
+++ src/frontend/constants.py
@@ -0,0 +1,12 @@
+ERROR_JOB_INVALID_ID           = 'Invalid job ID'
+ERROR_JOB_INVALID_PAYLOAD      = 'Decoding job data failed'
+ERROR_JOB_NOT_FOUND            = 'Job not found'
+ERROR_JOB_LOCKED               = 'Job locked'
+ERROR_JOB_CONFIG_WRITE_FAILED  = 'Failed to write job configuration'
+
+ERROR_TASK_INVALID_ID          = 'Invalid task ID'
+ERROR_TASK_INVALID_PAYLOAD     = 'Decoding task data failed'
+ERROR_TASK_NOT_FOUND           = 'Task not found'
+ERROR_TASK_LOCKED              = 'Task locked'
+ERROR_TASK_WRONG_ACTOR         = 'Task ownership mismatch'
+
