4e4b5b638ea1d87200196bb8fae239fd01976c3a
Heikki Nousiainen <Heikki.Nousiainen@F-Secure.com>
2013-03-04 18:11:52 +0200

    frontend: fix jobs/builds/artifacts handle_request prototype

--- src/frontend/jobs_builds_artifacts.py
+++ src/frontend/jobs_builds_artifacts.py
@@ -14,7 +14,7 @@ class JobsBuildsArtifacts(object):
         self.config = config
         self.log = logging.getLogger('jobs_builds_artifacts')
 
-    def handle_request(environ, start_response, method, job_id, build, parts):
+    def handle_request(self, environ, start_response, method, job_id, build, parts):
         self.log.debug('method %r job_id %r build %r', method, job_id, build)
         return response.send_error(start_response, 501)
 
