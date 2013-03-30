2212882080e2152e796908823d4065b4c1be3dfd
Heikki Nousiainen <heikki.nousiainen@f-secure.com>
2013-03-30 11:52:57 +0200

    setup: fix version capture to support Python 2.6 as well

--- version.py
+++ version.py
@@ -1,7 +1,12 @@
 import subprocess
 
 def git_version():
-    version_string = subprocess.check_output(['git', 'describe']).strip()
+    proc = subprocess.Popen(['git', 'describe'], stdout=subprocess.PIPE)
+    output, _ = proc.communicate()
+    if proc.returncode != 0:
+        raise Exception("Failed to run 'git describe'")
+
+    version_string = output.strip()
     file('src/version.py', 'wb').write('VERSION="%s"' % version_string)
     return version_string
 
