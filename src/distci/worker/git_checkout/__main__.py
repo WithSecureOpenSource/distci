8d71a647d8e3856f024e630a48b35175469d5a53
Heikki Nousiainen <heikki.nousiainen@f-secure.com>
2013-04-09 09:34:54 +0300

    worker: first version of git checkout worker

---
+++ src/distci/worker/git_checkout/__main__.py
@@ -0,0 +1,50 @@
+"""
+Entrypoint for git checkout worker
+
+Copyright (c) 2013 Heikki Nousiainen, F-Secure
+See LICENSE for details
+"""
+
+import json
+import optparse
+import sys
+import logging
+
+from . import git_checkout
+
+__appname__ = "git checkout worker"
+__usage__   = "%prog -c <configuration file>"
+__version__ = "1.0"
+__author__  = "Heikki Nousiainen"
+
+def main(argv):
+    """ Main function """
+    parser = optparse.OptionParser(description=__doc__, version=__version__)
+    parser.set_usage(__usage__)
+    parser.add_option("-c", dest="config_file", help="configuration filename")
+    opts, _ = parser.parse_args(argv)
+
+    if not opts.config_file:
+        print "configuration file not specified"
+        parser.print_help()
+        return -1
+
+    try:
+        config = json.load(file(opts.config_file, 'rb'))
+    except:
+        print "failed to parse configuration file"
+        return -1
+
+    if config.get('log_file'):
+        log_level = logging._levelNames.get(config.get('log_level', 'info').upper())
+        logging.basicConfig(level=log_level, format='%(asctime)s\t%(threadName)s\t%(name)s\t%(levelname)s\t%(message)s', filename=config.get('log_file'))
+
+    worker = git_checkout.GitCheckoutWorker(config)
+    return worker.start()
+
+def main_entry():
+    return main(sys.argv)
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
+
