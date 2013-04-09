bece1d993182b6e42cc95a05d5222bf69c28789a
Heikki Nousiainen <heikki.nousiainen@f-secure.com>
2013-04-09 16:06:52 +0300

    build: package the new workers into the resulting EGG

--- setup.py
+++ setup.py
@@ -8,11 +8,19 @@ setup(
                  'distci.frontend',
                  'distci.distcilib',
                  'distci.worker',
+                 'distci.worker.build_control',
+                 'distci.worker.git_checkout',
+                 'distci.worker.execute_shell',
+                 'distci.worker.publish_artifacts',
                  'distci.worker.calculator' ],
     package_dir = { 'distci': 'src/distci' },
     entry_points = {
         'console_scripts': [
             'distci-frontend = distci.frontend.__main__:main_entry',
+            'distci-build-control-worker = distci.worker.build_control.__main__:main_entry',
+            'distci-git-checkout-worker = distci.worker.git_checkout.__main__:main_entry',
+            'distci-execute-shell-worker = distci.worker.execute_shell.__main__:main_entry',
+            'distci-publish-artifacts-worker = distci.worker.publish_artifacts.__main__:main_entry',
             'distci-calculator-worker = distci.worker.calculator.__main__:main_entry'
         ]
     },
