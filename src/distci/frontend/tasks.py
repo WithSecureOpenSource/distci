445d1e3db885da41b69a7abe336994b1d71e816a
Heikki Nousiainen <heikki.nousiainen@f-secure.com>
2013-09-04 10:46:16 +0300

    frontend: maintain task state in zookeeper

--- src/distci/frontend/tasks.py
+++ src/distci/frontend/tasks.py
@@ -8,9 +8,7 @@ See LICENSE for details
 import os
 import uuid
 import json
-import shutil
 import logging
-import time
 import webob
 
 from distci.frontend import validators, sync, constants
@@ -22,6 +20,11 @@ class Tasks(object):
         self.zknodes = config.get('zookeeper_nodes', [])
         if len(self.zknodes) == 0:
             self.zknodes = None
+        else:
+            zk = sync.ZooKeeperData(self.zknodes)
+            zk.set('/distci', '')
+            zk.set('/distci/tasks', '')
+            zk.close()
 
     def _data_dir(self):
         """ Return tasks directory """
@@ -49,13 +52,15 @@ class Tasks(object):
 
     def get_tasks(self):
         """ Return information on all open tasks """
-        result = { 'tasks': [] }
-        task_ids = os.listdir(self._data_dir())
-        for task_id in task_ids:
-            if not os.path.isdir(self._task_dir(task_id)):
-                continue
-            result['tasks'].append(task_id)
+        if self.zknodes:
+            data_conn = sync.ZooKeeperData(self.zknodes, '/distci/tasks')
+        else:
+            data_conn = sync.FSData(self._data_dir())
+
+        result = {'tasks': data_conn.list() }
+        data_conn.close()
         return webob.Response(status=200, body=json.dumps(result), content_type="application/json")
+
 
     def create_new_task(self, request):
         """ Post a new task """
@@ -66,64 +71,76 @@ class Tasks(object):
             return webob.Response(status=400, body=constants.ERROR_TASK_INVALID_PAYLOAD)
 
         task_id_candidate = str(uuid.uuid4())
-        os.mkdir(self._task_dir(task_id_candidate))
-        self._save_task_config(task_id_candidate, task_description)
-        return webob.Response(status=201, body=json.dumps({'id': task_id_candidate, 'data': task_description}), content_type="application/json")
+        if self.zknodes:
+            data_conn = sync.ZooKeeperData(self.zknodes, '/distci/tasks')
+        else:
+            data_conn = sync.FSData(self._data_dir())
+
+        if data_conn.set('/%s' % task_id_candidate, json.dumps(task_description)) == True:
+            data_conn.close()
+            return webob.Response(status=201, body=json.dumps({'id': task_id_candidate, 'data': task_description}), content_type="application/json")
+        else:
+            data_conn.close()
+            # FIXME: error code
+            return webob.Response(status=500, body=constants.ERROR_TASK_LOCKED)
 
     def delete_task(self, task_id):
         """ Delete given task """
-        if not os.path.isdir(self._task_dir(task_id)):
-            return webob.Response(status=404, body=constants.ERROR_TASK_NOT_FOUND)
-        shutil.rmtree(self._task_dir(task_id))
+        if self.zknodes:
+            data_conn = sync.ZooKeeperData(self.zknodes, '/distci/tasks')
+        else:
+            data_conn = sync.FSData(self._data_dir())
+        data_conn.delete('/%s' % task_id)
+        data_conn.close()
         return webob.Response(status=204)
 
     def get_task(self, task_id):
         """ Return information on a specific task """
-        if not os.path.isdir(self._task_dir(task_id)):
+        if self.zknodes:
+            data_conn = sync.ZooKeeperData(self.zknodes, '/distci/tasks')
+        else:
+            data_conn = sync.FSData(self._data_dir())
+
+        task_data = data_conn.get('/%s' % task_id)
+        if task_data is None:
+            data_conn.close()
             return webob.Response(status=404, body=constants.ERROR_TASK_NOT_FOUND)
-        task_data = None
-        for _ in range(10):
-            try:
-                task_data = json.dumps(self._prepare_task_data(task_id))
-                break
-            except:
-                time.sleep(0.1)
-        if not task_data:
-            return webob.Response(status=409, body=constants.ERROR_TASK_LOCKED)
-        return webob.Response(status=200, body=task_data, content_type="application/json")
+        data_conn.close()
+        returned_data = {'id': task_id, 'data': json.loads(task_data) }
+        return webob.Response(status=200, body=json.dumps(returned_data), content_type="application/json")
 
     def update_task(self, request, task_id):
         """ Update data configuration for an existing task """
-        if not os.path.isdir(self._task_dir(task_id)):
+        if self.zknodes:
+            data_conn = sync.ZooKeeperData(self.zknodes, '/distci/tasks')
+        else:
+            data_conn = sync.FSData(self._data_dir())
+
+        task_data = data_conn.get('/%s' % task_id)
+        if task_data is None:
             return webob.Response(status=404, body=constants.ERROR_TASK_NOT_FOUND)
+
         try:
             new_task_description = json.load(request.body_file)
         except ValueError:
             self.log.error("Decoding task data failed '%s'" % task_id)
+            data_conn.close()
             return webob.Response(status=400, body=constants.ERROR_TASK_INVALID_PAYLOAD)
-        if self.zknodes:
-            lock = sync.ZooKeeperLock(self.zknodes, 'task-lock-%s' % task_id)
-        else:
-            lock = sync.PhonyLock('task-lock-%s' % task_id)
-        if lock.try_lock() != True:
-            lock.close()
-            self.log.warn("Task locked '%s'" % task_id)
-            return webob.Response(status=409, body=constants.ERROR_TASK_LOCKED)
         try:
-            old_task_description = self._load_task_config(task_id)
+            old_task_description = json.loads(task_data)
         except:
-            lock.unlock()
-            lock.close()
             self.log.error("Failed to read task data '%s'" % task_id)
+            data_conn.close()
             return webob.Response(status=500)
         if old_task_description.has_key('assignee') and new_task_description.has_key('assignee') and old_task_description['assignee'] != new_task_description['assignee']:
-            lock.unlock()
-            lock.close()
+            data_conn.close()
             self.log.info("Task assignment conflict '%s'" % task_id)
             return webob.Response(status=409, body=constants.ERROR_TASK_WRONG_ACTOR)
-        self._save_task_config(task_id, new_task_description)
-        lock.unlock()
-        lock.close()
+        if data_conn.set('/%s' % task_id, json.dumps(new_task_description), task_data) == False:
+            data_conn.close()
+            return webob.Response(status=409, body=constants.ERROR_TASK_LOCKED)
+
+        data_conn.close()
         return webob.Response(status=200, body=json.dumps({'id': task_id, 'data': new_task_description}), content_type="application/json")
 
     def handle_request(self, request, parts):
